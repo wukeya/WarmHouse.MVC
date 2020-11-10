3e694c4df16194e023c299898d148d8dda8e526f
BaiMei0320 <74173146+BaiMei0320@users.noreply.github.com>
2020-11-10 15:47:42 +0800

    界面控制器添加

--- UI.MVC/Controllers/UserController.cs
+++ UI.MVC/Controllers/UserController.cs
@@ -17,11 +17,48 @@ namespace UI.MVC.Controllers
         {
             return View();
         }
-        //////////////登录页面
+        //登录页面
         //首页
         public IActionResult First()
         {
             return View();
+        }
+
+        //--------------------
+
+        //一对多添加
+        public IActionResult ADD()
+        {
+            return View();
+        }
+        //审核
+        public IActionResult Audit()
+        {
+            return View();
+        }
+        //入库
+        public IActionResult EnterLibrary()
+        {
+            return View();
+
+        }
+        //出库
+        public IActionResult OutLibrary()
+        {
+            return View();
+
+        }
+        //调库
+        public IActionResult ChangeLibrary()
+        {
+            return View();
+
+        }
+        //报损
+        public IActionResult Breakage()
+        {
+            return View();
+
         }
     }
 }
